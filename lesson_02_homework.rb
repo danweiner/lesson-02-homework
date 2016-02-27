a7c03716dd0fc7eef6ceae62a1d705b563dabffd
Dan Weiner <daweiner16@gmail.com>
2016-02-27 15:45:08 -0500

    deleted party name attribute, deleted extra instances

--- lesson_02_homework.rb
+++ lesson_02_homework.rb
@@ -5,54 +5,53 @@
 # If it is, it returns a specific word
 # If it isn't, it returns the number
 
-def fizzbuzz(num)
-  # Example comment line
+# def fizzbuzz(num)
+#   # Example comment line
 
-# This works like an if statement
-  case
+# # This works like an if statement
+#   case
 
-    # If the number is divisible by 15 then return "FizzBuzz"
-    when num % 15 == 0 then "FizzBuzz"
-    # If the number is divisible by 5 then return "Fizz"  
-    when num % 3  == 0 then "Fizz"
-    # If the number is divisible by 3 then return "Buzz"
-    when num % 5  == 0 then "Buzz"
-    # If the number is not divisible by 15, 5, or 3, just return the number itself
-    else num
-  end
-end
+#     # If the number is divisible by 15 then return "FizzBuzz"
+#     when num % 15 == 0 then "FizzBuzz"
+#     # If the number is divisible by 5 then return "Fizz"  
+#     when num % 3  == 0 then "Fizz"
+#     # If the number is divisible by 3 then return "Buzz"
+#     when num % 5  == 0 then "Buzz"
+#     # If the number is not divisible by 15, 5, or 3, just return the number itself
+#     else num
+#   end
+# end
 
-# This is a loop
-def fizz_buzz_to(limit)
-  # starting at 1, up to the parameter limit, run the method fizzbuzz
-  1.upto(limit).each do |num|
-    puts fizzbuzz(num)
-  end
-end
+# # This is a loop
+# def fizz_buzz_to(limit)
+#   # starting at 1, up to the parameter limit, run the method fizzbuzz
+#   1.upto(limit).each do |num|
+#     puts fizzbuzz(num)
+#   end
+# end
 
-puts fizzbuzz(10)
-puts fizz_buzz_to(20)
+# puts fizzbuzz(10)
+# puts fizz_buzz_to(20)
 
 
-# Fix Broken Code
+# # Fix Broken Code
 
-def area_of_triangle(base, height)
-  base * height / 2
-end
+# def area_of_triangle(base, height)
+#   base * height / 2
+# end
 
-base = 7
-height = 6
+# base = 7
+# height = 6
 
-puts area_of_triangle(base, height)
+# puts area_of_triangle(base, height)
 
 # Coding
 
 class Waitlist
-  attr_accessor :party_name
+  
   attr_accessor :list
 
   def initialize
-    @party_name = party_name
     @list = []
   end
 
@@ -68,7 +67,7 @@ class Waitlist
       # puts "hello #{name}"
       name
     end
-    puts @list.inspect
+    @list.inspect
   end
 
   def seat
@@ -77,19 +76,15 @@ class Waitlist
       # puts "hello #{name}"
       name
     end
-    puts @list.inspect
+    @list.inspect
   end
 
 end
 
-waitlist = Waitlist.new
-waitlist = Waitlist.new
-waitlist = Waitlist.new
-waitlist = Waitlist.new
-waitlist = Waitlist.new
+
 waitlist = Waitlist.new
 
-puts waitlist.add_party("Danny")
+
 puts waitlist.add_party("Steve")
 puts waitlist.add_party("Tom")
 puts waitlist.add_party("Barry")
